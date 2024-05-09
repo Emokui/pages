d316ae30aaf53402ca40966cd621316a30f8c8f5
Emokui <136175327+Emokui@users.noreply.github.com>
2024-05-09 09:08:09 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,414 @@
+// src/worker.js
+import { connect } from "cloudflare:sockets";
+//自行设置password
+let sha224Password = '3e07a2a64848d50002ee4af0622c53a9e06f299ff10470b804086053';//password加密后sha224值
+let password= '1101';//password与sha224加密值必须一致
+//伪装web
+let proxydomain = 'www.bing.com';
+//proxyip
+let proxyIP = 'proxyip.fxxk.dedyn.io';
+let RproxyIP = 'true';//设为true则强制使用订阅器内置的proxyIP
+//自行设置TOKEN
+let token= 'auto';
+//内置订阅器嵌套
+let sub = 'sub.fbi.cloudns.be';//订阅器
+let subconverter = 'apiurl.v1.mk';//转换后端
+let subconfig = 'https://raw.githubusercontent.com/JustLagom/test/main/urltestconfig.ini';//配置文件config
+
+if (!isValidSHA224(sha224Password)) {
+    throw new Error('sha224Password is not valid');
+}
+
+const worker_default = {
+    /**
+     * @param {import("@cloudflare/workers-types").Request} request
+     * @param {{TOKEN, PASSWORD, SHA224PASS, PROXYIP, PROXYDOMAIN, RPROXYIP, SUB, SUBAPI, SUBCONFIG: string}} env
+     * @param {import("@cloudflare/workers-types").ExecutionContext} ctx
+     * @returns {Promise<Response>}
+     */
+    async fetch(request, env, ctx) {
+        try {
+            token = env.TOKEN || token;
+            password = env.PASSWORD || password;
+            sha224Password = env.SHA224PASS || sha224Password
+            proxyIP = env.PROXYIP || proxyIP;
+            proxydomain = env.PROXYDOMAIN || proxydomain;
+            RproxyIP = env.RPROXYIP || RproxyIP;
+            sub = env.SUB || sub;
+            subconverter = env.SUBAPI || subconverter;
+            subconfig = env.SUBCONFIG || subconfig;
+            const UA = request.headers.get('User-Agent') || 'null';
+            const userAgent = UA.toLowerCase();
+            const upgradeHeader = request.headers.get("Upgrade");
+            const url = new URL(request.url);
+            if (!upgradeHeader || upgradeHeader !== "websocket") {
+                //const url = new URL(request.url);
+                switch (url.pathname.toLowerCase()) {
+                    case `/${token}`: {
+                        const trojanConfig = await getTROJANConfig(password, request.headers.get('Host'), sub, UA, RproxyIP, url);
+                        const now = Date.now();
+                        const timestamp = Math.floor(now / 1000);
+                        const expire = 4102329600;//2099-12-31
+                        const today = new Date(now);
+                        today.setHours(0, 0, 0, 0);
+                        const UD = Math.floor(((now - today.getTime())/86400000) * 24 * 1099511627776 / 2);
+                        if (userAgent && userAgent.includes('mozilla')){
+                        	return new Response(`${trojanConfig}`, {
+                        		status: 200,
+                        		headers: {
+                        			"Content-Type": "text/plain;charset=utf-8",
+                        		}
+                        	});
+                        } else {
+                        	return new Response(`${trojanConfig}`, {
+                        		status: 200,
+                        		headers: {
+                        			"Content-Disposition": "attachment; filename=TrojanConfig; filename*=utf-8''TrojanConfig",
+                        			"Content-Type": "text/plain;charset=utf-8",
+                        			"Profile-Update-Interval": "6",
+                        			"Subscription-Userinfo": `upload=${UD}; download=${UD}; total=${24 * 1099511627776}; expire=${expire}`,
+                        		}
+                        	});
+                        }
+                    }
+                    default:
+                         url.hostname = proxydomain;
+                         url.protocol = 'https:';
+                         request = new Request(url, request);
+                         return await fetch(request);
+                      }
+            } else {
+                proxyIP = url.searchParams.get('proxyip') || proxyIP;
+                if (new RegExp('/proxyip=', 'i').test(url.pathname)) proxyIP = url.pathname.toLowerCase().split('/proxyip=')[1];
+                else if (new RegExp('/proxyip.', 'i').test(url.pathname)) proxyIP = `proxyip.${url.pathname.toLowerCase().split("/proxyip.")[1]}`;
+                else if (!proxyIP || proxyIP == '') proxyIP = 'proxyip.fxxk.dedyn.io';
+                return await trojanOverWSHandler(request);
+            }
+        } catch (err) {
+            let e = err;
+            return new Response(e.toString());
+        }
+    }
+};
+
+async function trojanOverWSHandler(request) {
+    const webSocketPair = new WebSocketPair();
+    const [client, webSocket] = Object.values(webSocketPair);
+    webSocket.accept();
+    let address = "";
+    let portWithRandomLog = "";
+    const log = (info, event) => {
+        console.log(`[${address}:${portWithRandomLog}] ${info}`, event || "");
+    };
+    const earlyDataHeader = request.headers.get("sec-websocket-protocol") || "";
+    const readableWebSocketStream = makeReadableWebSocketStream(webSocket, earlyDataHeader, log);
+    let remoteSocketWapper = {
+        value: null
+    };
+    let udpStreamWrite = null;
+    readableWebSocketStream.pipeTo(new WritableStream({
+        async write(chunk, controller) {
+            if (udpStreamWrite) {
+                return udpStreamWrite(chunk);
+            }
+            if (remoteSocketWapper.value) {
+                const writer = remoteSocketWapper.value.writable.getWriter();
+                await writer.write(chunk);
+                writer.releaseLock();
+                return;
+            }
+            const {
+                hasError,
+                message,
+                portRemote = 443,
+                addressRemote = "",
+                rawClientData
+            } = await parseTrojanHeader(chunk);
+            address = addressRemote;
+            portWithRandomLog = `${portRemote}--${Math.random()} tcp`;
+            if (hasError) {
+                throw new Error(message);
+                return;
+            }
+            handleTCPOutBound(remoteSocketWapper, addressRemote, portRemote, rawClientData, webSocket, log);
+        },
+        close() {
+            log(`readableWebSocketStream is closed`);
+        },
+        abort(reason) {
+            log(`readableWebSocketStream is aborted`, JSON.stringify(reason));
+        }
+    })).catch((err) => {
+        log("readableWebSocketStream pipeTo error", err);
+    });
+    return new Response(null, {
+        status: 101,
+        // @ts-ignore
+        webSocket: client
+    });
+}
+
+async function parseTrojanHeader(buffer) {
+    if (buffer.byteLength < 56) {
+        return {
+            hasError: true,
+            message: "invalid data"
+        };
+    }
+    let crLfIndex = 56;
+    if (new Uint8Array(buffer.slice(56, 57))[0] !== 0x0d || new Uint8Array(buffer.slice(57, 58))[0] !== 0x0a) {
+        return {
+            hasError: true,
+            message: "invalid header format (missing CR LF)"
+        };
+    }
+    const password = new TextDecoder().decode(buffer.slice(0, crLfIndex));
+    if (password !== sha224Password) {
+        return {
+            hasError: true,
+            message: "invalid password"
+        };
+    }
+
+    const socks5DataBuffer = buffer.slice(crLfIndex + 2);
+    if (socks5DataBuffer.byteLength < 6) {
+        return {
+            hasError: true,
+            message: "invalid SOCKS5 request data"
+        };
+    }
+
+    const view = new DataView(socks5DataBuffer);
+    const cmd = view.getUint8(0);
+    if (cmd !== 1) {
+        return {
+            hasError: true,
+            message: "unsupported command, only TCP (CONNECT) is allowed"
+        };
+    }
+
+    const atype = view.getUint8(1);
+    // 0x01: IPv4 address
+    // 0x03: Domain name
+    // 0x04: IPv6 address
+    let addressLength = 0;
+    let addressIndex = 2;
+    let address = "";
+    switch (atype) {
+        case 1:
+            addressLength = 4;
+            address = new Uint8Array(
+              socks5DataBuffer.slice(addressIndex, addressIndex + addressLength)
+            ).join(".");
+            break;
+        case 3:
+            addressLength = new Uint8Array(
+              socks5DataBuffer.slice(addressIndex, addressIndex + 1)
+            )[0];
+            addressIndex += 1;
+            address = new TextDecoder().decode(
+              socks5DataBuffer.slice(addressIndex, addressIndex + addressLength)
+            );
+            break;
+        case 4:
+            addressLength = 16;
+            const dataView = new DataView(socks5DataBuffer.slice(addressIndex, addressIndex + addressLength));
+            const ipv6 = [];
+            for (let i = 0; i < 8; i++) {
+                ipv6.push(dataView.getUint16(i * 2).toString(16));
+            }
+            address = ipv6.join(":");
+            break;
+        default:
+            return {
+                hasError: true,
+                message: `invalid addressType is ${atype}`
+            };
+    }
+
+    if (!address) {
+        return {
+            hasError: true,
+            message: `address is empty, addressType is ${atype}`
+        };
+    }
+
+    const portIndex = addressIndex + addressLength;
+    const portBuffer = socks5DataBuffer.slice(portIndex, portIndex + 2);
+    const portRemote = new DataView(portBuffer).getUint16(0);
+    return {
+        hasError: false,
+        addressRemote: address,
+        portRemote,
+        rawClientData: socks5DataBuffer.slice(portIndex + 4)
+    };
+}
+
+async function handleTCPOutBound(remoteSocket, addressRemote, portRemote, rawClientData, webSocket, log) {
+    async function connectAndWrite(address, port) {
+        const tcpSocket2 = connect({
+            hostname: address,
+            port
+        });
+        remoteSocket.value = tcpSocket2;
+        log(`connected to ${address}:${port}`);
+        const writer = tcpSocket2.writable.getWriter();
+        await writer.write(rawClientData);
+        writer.releaseLock();
+        return tcpSocket2;
+    }
+    async function retry() {
+        const tcpSocket2 = await connectAndWrite(proxyIP || addressRemote, portRemote);
+        tcpSocket2.closed.catch((error) => {
+            console.log("retry tcpSocket closed error", error);
+        }).finally(() => {
+            safeCloseWebSocket(webSocket);
+        });
+        remoteSocketToWS(tcpSocket2, webSocket, null, log);
+    }
+    const tcpSocket = await connectAndWrite(addressRemote, portRemote);
+    remoteSocketToWS(tcpSocket, webSocket, retry, log);
+}
+
+function makeReadableWebSocketStream(webSocketServer, earlyDataHeader, log) {
+    let readableStreamCancel = false;
+    const stream = new ReadableStream({
+        start(controller) {
+            webSocketServer.addEventListener("message", (event) => {
+                if (readableStreamCancel) {
+                    return;
+                }
+                const message = event.data;
+                controller.enqueue(message);
+            });
+            webSocketServer.addEventListener("close", () => {
+                safeCloseWebSocket(webSocketServer);
+                if (readableStreamCancel) {
+                    return;
+                }
+                controller.close();
+            });
+            webSocketServer.addEventListener("error", (err) => {
+                log("webSocketServer error");
+                controller.error(err);
+            });
+            const { earlyData, error } = base64ToArrayBuffer(earlyDataHeader);
+            if (error) {
+                controller.error(error);
+            } else if (earlyData) {
+                controller.enqueue(earlyData);
+            }
+        },
+        pull(controller) {},
+        cancel(reason) {
+            if (readableStreamCancel) {
+                return;
+            }
+            log(`readableStream was canceled, due to ${reason}`);
+            readableStreamCancel = true;
+            safeCloseWebSocket(webSocketServer);
+        }
+    });
+    return stream;
+}
+
+async function remoteSocketToWS(remoteSocket, webSocket, retry, log) {
+    let hasIncomingData = false;
+    await remoteSocket.readable.pipeTo(
+        new WritableStream({
+            start() {},
+            /**
+             *
+             * @param {Uint8Array} chunk
+             * @param {*} controller
+             */
+            async write(chunk, controller) {
+                hasIncomingData = true;
+                if (webSocket.readyState !== WS_READY_STATE_OPEN) {
+                    controller.error(
+                        "webSocket connection is not open"
+                    );
+                }
+                webSocket.send(chunk);
+            },
+            close() {
+                log(`remoteSocket.readable is closed, hasIncomingData: ${hasIncomingData}`);
+            },
+            abort(reason) {
+                console.error("remoteSocket.readable abort", reason);
+            }
+        })
+    ).catch((error) => {
+        console.error(
+            `remoteSocketToWS error:`,
+            error.stack || error
+        );
+        safeCloseWebSocket(webSocket);
+    });
+    if (hasIncomingData === false && retry) {
+        log(`retry`);
+        retry();
+    }
+}
+
+function isValidSHA224(hash) {
+    const sha224Regex = /^[0-9a-f]{56}$/i;
+    return sha224Regex.test(hash);
+}
+
+function base64ToArrayBuffer(base64Str) {
+    if (!base64Str) {
+        return { error: null };
+    }
+    try {
+        base64Str = base64Str.replace(/-/g, "+").replace(/_/g, "/");
+        const decode = atob(base64Str);
+        const arryBuffer = Uint8Array.from(decode, (c) => c.charCodeAt(0));
+        return { earlyData: arryBuffer.buffer, error: null };
+    } catch (error) {
+        return { error };
+    }
+}
+
+let WS_READY_STATE_OPEN = 1;
+let WS_READY_STATE_CLOSING = 2;
+
+function safeCloseWebSocket(socket) {
+    try {
+        if (socket.readyState === WS_READY_STATE_OPEN || socket.readyState === WS_READY_STATE_CLOSING) {
+            socket.close();
+        }
+    } catch (error) {
+        console.error("safeCloseWebSocket error", error);
+    }
+}
+export {
+    worker_default as
+    default
+};
+//# sourceMappingURL=worker.js.map
+
+/**
+ * @param {string} password
+ * @param {string | null} hostName
+ * @param {string} sub
+ * @param {string} UA
+ * @returns {Promise<string>}
+ */
+let subParams = ['sub','base64','b64','clash','singbox','sb'];
+async function getTROJANConfig(password, hostName, sub, UA, RproxyIP, _url) {
+	const userAgent = UA.toLowerCase();
+	if ((!sub || sub === '' || (sub && userAgent.includes('mozilla'))) && !subParams.some(_searchParams => _url.searchParams.has(_searchParams))) {
+    return `
+    <p>===================================================配置详解=======================================================</p>
+    Subscribe / sub 订阅地址, 支持 Base64、clash-meta、sing-box 订阅格式, 您的订阅内容由 ${sub} 提供维护支持, 自动获取ProxyIP: ${RproxyIP}.
+    --------------------------------------------------------------------------------------------------------------------
+    订阅地址：https://${sub}/sub?host=${hostName}&password=${password}&proxyip=${RproxyIP}
+    <p>=================================================================================================================</p>
+    github 项目地址 Star!Star!Star!!!
+    telegram 交流群 技术大佬~在线发牌!
+    https://t.me/CMLiussss
+    <p>=================================================================================================================</p>
+    `
+  }
+}
